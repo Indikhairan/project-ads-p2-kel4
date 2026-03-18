d4fb5a2e830061ab01d86208d899e0760797a231
muthica <ccmuthia@apps.ipb.ac.id>
2026-03-18 16:04:38 +0700

    Update nama import di security.py

--- backend/security.py
+++ backend/security.py
@@ -3,7 +3,7 @@ import os
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from fastapi import HTTPException, status
-from database import db
+from backend.database import get_db
 
 load_dotenv()
 
