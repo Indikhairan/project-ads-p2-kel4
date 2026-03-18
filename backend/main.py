c16c98b28be793edd250a3dd1c192740a886a9ff
muthica <ccmuthia@apps.ipb.ac.id>
2026-03-18 15:27:01 +0700

    Update comments

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,10 @@ from fastapi import FastAPI
 from backend.routers import tiket
 from backend.database import engine
 from backend import models
-# Jika kamu pakai folder 'routers'
-# ATAU jika file tiket.py langsung di folder backend:
-# import tiket 
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Menghubungkan router ke aplikasi utama
+# Menghubungkan router ke main app
 app.include_router(tiket.router)
