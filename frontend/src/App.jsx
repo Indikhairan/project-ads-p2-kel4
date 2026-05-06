014de0e41aa06a8a8f133fafc9ef64e2105399ff
Indriyani Khairan Nisa <email.github.indikhairan@apps.ipb.ac.id.com>
2026-05-06 21:37:43 +0700

    feat: login page dan homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login'; 
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { LoginPage } from "./pages/login";
+import { HomepageMahasiswa } from "./pages/HomepageMahasiswa";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        {/* Path "/" adalah halaman pertama kali web dibuka (Login) */}
+        <Route path="/" element={<LoginPage />} />
+        
+        {/* Path "/dashboard" adalah halaman setelah berhasil login */}
+        <Route path="/dashboard" element={<HomepageMahasiswa />} />
       </Routes>
     </BrowserRouter>
   );
